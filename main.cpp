c226fb0d008560542edb411dd387529667b4857e
brs.lf <876338266@qq.com>
2025-05-15 23:37:19 +0800

    feat: Qt项目初始化，ffmpeglib集成至QT项目

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include <QApplication>
+// 为了使用qDebug函数
+#include <QDebug>
+
+// FFmpeg是C语言库
+// 有了extern "C"，才能在C++中导入C语言函数
+extern "C" {
+#include "libavutil/avutil.h"
+}
+
+int main(int argc, char* argv[])
+{
+    // 打印版本信息
+    qDebug() << av_version_info();
+
+    QApplication a(argc, argv);
+    return a.exec();
+}
